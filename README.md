562677111e05c0f07b6e4f5b4463b515da83eaee
Michael Chong <daohanchong@gmail.com>
2022-11-04 07:07:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# BazelGen
+
+Generate Bazel BUILD files from Xcode projects.
+
+## License
+
+BazelGen is released under the MIT license. See LICENSE for details.
---
+++ Sources/BazelGen/BazelGen.swift
@@ -0,0 +1,8 @@
+@main
+public struct BazelGen {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(BazelGen().text)
+    }
+}
