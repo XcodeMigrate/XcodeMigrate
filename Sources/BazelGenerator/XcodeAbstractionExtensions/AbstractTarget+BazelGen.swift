8db37fe2d600f8df9cf2d2e2ed1aaba861c429bd
imWildCat <wildcat.name@gmail.com>
2022-12-26 15:03:39 -0800

    add phone os app target

--- Sources/BazelGenerator/XcodeAbstractionExtensions/AbstractTarget+BazelGen.swift
+++ Sources/BazelGenerator/XcodeAbstractionExtensions/AbstractTarget+BazelGen.swift
@@ -2,36 +2,47 @@ import FoundationExtension
 import XcodeAbstraction
 
 enum BazelGeneratorAbstractTargetError: Error {
-    case unimplemented
+  case unimplemented
 }
 
 extension AbstractTarget {
-    func bazelGen() throws -> String {
-        switch productType {
-        case .framework:
-            return generateSwiftLibrary()
-        case .application:
-            fallthrough
-        case .unitTestBundle:
-            fallthrough
-        case .uiTestBundle:
-            fallthrough
+  func bazelGen() throws -> String {
+    switch productType {
+    case .framework:
+      return generateSwiftLibrary()
+    case .application:
+      return generatePhoneOSApplication()
+    case .unitTestBundle:
+      fallthrough
+    case .uiTestBundle:
+      fallthrough
 
-        default:
-            throw BazelGeneratorAbstractTargetError.unimplemented
-        }
+    default:
+      throw BazelGeneratorAbstractTargetError.unimplemented
     }
+  }
 }
 
 private extension AbstractTarget {
-    func generateSwiftLibrary() -> String {
-        let sourcePaths = sourceFiles.map { $0.path.string }
+  func generateSwiftLibrary() -> String {
+    let sourcePaths = sourceFiles.map { $0.path.string }
 
-        return """
-        swift_library(
-            name = "\(name)",
-            srcs = \(sourcePaths.toArrayLiteralString())
-        )
-        """
-    }
+    return """
+    swift_library(
+        name = "\(name)",
+        srcs = \(sourcePaths.toArrayLiteralString())
+    )
+    """
+  }
+
+  func generatePhoneOSApplication() -> String {
+    let sourcePaths = sourceFiles.map { $0.path.string }
+
+    return """
+    ios_application(
+        name = "\(name)",
+        srcs = \(sourcePaths.toArrayLiteralString())
+    )
+    """
+  }
 }
