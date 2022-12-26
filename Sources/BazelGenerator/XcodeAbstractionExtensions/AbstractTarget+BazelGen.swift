9247356cf7ece33c810b94b1a32cd53e22e0ecd2
imWildCat <wildcat.name@gmail.com>
2022-12-26 15:45:50 -0800

    add deps to app target

--- Sources/BazelGenerator/XcodeAbstractionExtensions/AbstractTarget+BazelGen.swift
+++ Sources/BazelGenerator/XcodeAbstractionExtensions/AbstractTarget+BazelGen.swift
@@ -2,56 +2,61 @@ import FoundationExtension
 import XcodeAbstraction
 
 enum BazelGeneratorAbstractTargetError: Error {
-  case unimplemented
+    case unimplemented
 }
 
 extension AbstractTarget {
-  func bazelGen() throws -> String {
-    switch productType {
-    case .framework:
-      return generateSwiftLibrary()
-    case .application:
-      return generatePhoneOSApplication()
-    case .unitTestBundle:
-      fallthrough
-    case .uiTestBundle:
-      fallthrough
-
-    default:
-      throw BazelGeneratorAbstractTargetError.unimplemented
+    func bazelGen() throws -> String {
+        switch productType {
+        case .framework:
+            return generateSwiftLibrary()
+        case .application:
+            return generatePhoneOSApplication()
+        case .unitTestBundle:
+            fallthrough
+        case .uiTestBundle:
+            fallthrough
+        default:
+            throw BazelGeneratorAbstractTargetError.unimplemented
+        }
     }
-  }
 }
 
 private extension AbstractTarget {
-  func generateSwiftLibrary() -> String {
-    let sourcePaths = sourceFiles.map { $0.path.string }
-    let dependencyLabels = dependencies.map { dependency in
-      let commonPathPrefix = String.commonPrefix(strings: [
-        self.path.string,
-        dependency.path.string,
-      ])
-      let dependencyLabelWithOutCommonPath = dependency.path.string[commonPathPrefix.endIndex...]
-      return "//\(dependencyLabelWithOutCommonPath)"
+    func generateSwiftLibrary() -> String {
+        let sourcePaths = sourceFiles.map { $0.path.string }
+
+        return """
+        swift_library(
+            name = "\(name)",
+            srcs = \(sourcePaths.toArrayLiteralString()),
+            deps = \(dependencyLabels.toArrayLiteralString())
+        )
+        """
     }
 
-    return """
-    swift_library(
-        name = "\(name)",
-        srcs = \(sourcePaths.toArrayLiteralString()),
-        deps = \(dependencyLabels.toArrayLiteralString())
-    )
-    """
-  }
+    func generatePhoneOSApplication() -> String {
+        let sourcePaths = sourceFiles.map { $0.path.string }
 
-  func generatePhoneOSApplication() -> String {
-    let sourcePaths = sourceFiles.map { $0.path.string }
+        return """
+        ios_application(
+            name = "\(name)",
+            srcs = \(sourcePaths.toArrayLiteralString()),
+            deps = \(dependencyLabels.toArrayLiteralString())
+        )
+        """
+    }
+}
 
-    return """
-    ios_application(
-        name = "\(name)",
-        srcs = \(sourcePaths.toArrayLiteralString())
-    )
-    """
-  }
+private extension AbstractTarget {
+    var dependencyLabels: [String] {
+        return dependencies.map { dependency in
+            let commonPathPrefix = String.commonPrefix(strings: [
+                self.path.string,
+                dependency.path.string,
+            ])
+            let dependencyLabelWithOutCommonPath = dependency.path.string[commonPathPrefix.endIndex...]
+            return "//\(dependencyLabelWithOutCommonPath)"
+        }
+    }
 }
