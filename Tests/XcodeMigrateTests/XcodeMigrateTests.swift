1eabe6079f1a944493528e402e33dc6f481b7c8d
imWildCat <wildcat.name@gmail.com>
2022-12-28 21:26:15 -0800

    update

--- Tests/XcodeMigrateTests/XcodeMigrateTests.swift
+++ Tests/XcodeMigrateTests/XcodeMigrateTests.swift
@@ -2,8 +2,10 @@
 import XCTest
 
 final class XcodeMigrateTests: XCTestCase {
+    // swiftlint:disable empty_xctest_method
     func testExample() throws {
 //        var xcodeMigrate = XcodeMigrate()
 //        try! xcodeMigrate.run()
     }
+    // swiftlint:enable empty_xctest_method
 }
