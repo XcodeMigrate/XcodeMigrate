1fc640bd92e58e77483c0c45b7d9e7328af2498c
imWildCat <wildcat.name@gmail.com>
2022-12-28 21:11:34 -0800

    remove empty test

--- Tests/XcodeMigrateTests/XcodeMigrateTests.swift
+++
@@ -1,9 +0,0 @@
-@testable import XcodeMigrate
-import XCTest
-
-final class XcodeMigrateTests: XCTestCase {
-    func testExample() throws {
-//        var xcodeMigrate = XcodeMigrate()
-//        try! xcodeMigrate.run()
-    }
-}
