4f9b07b40669851d24395a9af299f7e9b66ba4c2
imWildCat <wildcat.name@gmail.com>
2022-12-30 20:41:30 -0800

    bazelkit

---
+++ Sources/BazelKit/BazelRuleRepresentable.swift
@@ -0,0 +1,12 @@
+//
+//  BazelRuleRepresentable.swift
+//  
+//
+//  Created by Daohan Chong on 12/30/22.
+//
+
+import Foundation
+
+internal protocol BazelRuleRepresentable {
+    var name: String { get }
+}
---
+++ Tests/BazelKitTests/BazelKitTests.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by WildCat on 12/30/22.
+//
+
+import Foundation
